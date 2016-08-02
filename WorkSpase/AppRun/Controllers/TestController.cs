d7aeab0b5919472244b8f852c553680298553fd8
Alexey Stupakov [KirkAwo] <alexey.stupakov@gmail.com>
2016-08-02 15:39:29 +0300

    Create new branch and start working

--- WorkSpase/AppRun/Controllers/TestController.cs
+++ WorkSpase/AppRun/Controllers/TestController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AppRun.Models;
 
-// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AppRun.Controllers
 {
